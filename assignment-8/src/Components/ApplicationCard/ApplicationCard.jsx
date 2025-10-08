8437efb017e72c65efcce96b316b07c43cc21648
antnose <antnose28@gmail.com>
2025-10-08 10:53:22 +0600

    Fixed card data

--- assignment-8/src/Components/ApplicationCard/ApplicationCard.jsx
+++ assignment-8/src/Components/ApplicationCard/ApplicationCard.jsx
@@ -1,5 +1,17 @@
 import { Link } from "react-router";
+import downloadImg from "../../assets/icon-downloads.png";
+import reviewdImg from "../../assets/icon-ratings.png";
+
 const ApplicationCard = ({ data }) => {
+  // SHow download number in understandable way start
+  const formatDownloads = (num) => {
+    if (!num) return "0";
+    return new Intl.NumberFormat("en", {
+      notation: "compact",
+      compactDisplay: "short",
+    }).format(num);
+  };
+  // SHow download number in understandable way End
   return (
     <Link
       to={`/allApps/${data?.id}`}
@@ -15,11 +27,13 @@ const ApplicationCard = ({ data }) => {
       <div className="card-body">
         <h2 className="card-title"> {data?.title} </h2>
         <div className="card-actions flex justify-between">
-          <div className="badge badge-outline border-0 lg:p-4 badge-success bg-green-100">
-            Success
+          <div className="badge badge-outline border-0 lg:p-4 badge-success bg-green-100 flex items-center justify-center">
+            <img className="w-4 h-4" src={downloadImg} alt="" />
+            {formatDownloads(data?.downloads)}
           </div>
-          <div className="badge badge-soft lg:p-4 badge-warning bg-amber-100 ">
-            Warning
+          <div className="badge badge-soft lg:p-4 badge-warning bg-amber-100 flex items-center justify-center ">
+            <img className="w-4 h-4" src={reviewdImg} alt="" />
+            {formatDownloads(data?.ratingAvg)}
           </div>
         </div>
       </div>
